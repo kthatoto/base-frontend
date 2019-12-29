18202357aa8fd5648247bab138923709a3f8bad7
kthatoto <kthatoto@gmail.com>
2019-12-30 04:13:12 +0900

    Fix env variable

--- src/plugins/apiClient.js
+++ src/plugins/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const apiClient = (method, url, data = null) => {
   return axios({
     method,
-    url: process.env.serverUrl + url,
+    url: process.env.SERVER_URL + url,
     data,
     headers: { Authorization: localStorage.jwt ? `Bearer ${localStorage.jwt}` : null }
   }).catch((err) => { return err.response })
